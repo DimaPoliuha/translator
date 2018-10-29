4bd3a9dabdc04844ad8a6044a447b4243ee3be6b
DmitryPoliuha <dmitry.polyuha@gmail.com>
2018-10-29 19:07:51 +0200

    except re-announcement and using undeclared identifier. add idn id to lexem table

--- lexical_analyzer/add_token.py
+++ lexical_analyzer/add_token.py
@@ -1,4 +1,5 @@
 import csv
+from lexical_analyzer.tokens_identifiers import tokens_identifiers
 
 root_dir = './'
 tables_path = root_dir + 'tables/'
@@ -22,28 +23,49 @@ def add_token(tok, token_type, program_file_name, line):
     with open(tables_path + program_file_name + '/tokens.csv', 'a', newline='') as f:
         writer = csv.writer(f)
 
-        if token_type == 'IDN':
-            writer.writerow([token_count, line, tok, 'id', '', '', 100])
+        if tok in tokens_identifiers:
+            writer.writerow([token_count, line, tok, '', '', '', tokens_identifiers[tok]])
+
+        elif token_type == 'IDN':
+            # read IDNs from file
+            with open(tables_path + program_file_name + '/IDN.csv', 'r', newline='') as file_identifiers:
+                reader = csv.reader(file_identifiers, delimiter=',')
+                idns = []
+                for row in reader:
+                    if row[0] == 'Id':
+                        continue
+                    idns.append(row[1])
+            if announcements_block and tok in idns:
+                raise Exception('Re-announcement')
+            elif not announcements_block and tok not in idns:
+                raise Exception('Use of undeclared identifier')
             # write to IDN file
-            with open(tables_path + program_file_name + '/IDN.csv', 'a', newline='') as file_identifiers:
-                writer_identifiers = csv.writer(file_identifiers)
-                writer_identifiers.writerow([idn_count, tok, 'type', 'value'])
-            idn_count += 1
+            idn_id = idns.index(tok) if tok in idns else idn_count
+            if announcements_block:
+                with open(tables_path + program_file_name + '/IDN.csv', 'a', newline='') as file_identifiers:
+                    writer_identifiers = csv.writer(file_identifiers)
+                    writer_identifiers.writerow([idn_count, tok, 'type', 'value'])
+                idn_count += 1
+
+            # write to tokens file
+            writer.writerow([token_count, line, tok, idn_id, '', '', 100])
 
         elif token_type == 'CON':
-            writer.writerow([token_count, line, tok, '', 'id', '', 101])
             # write to CONST file
             with open(tables_path + program_file_name + '/CONST.csv', 'a', newline='') as file_constants:
                 writer_constants = csv.writer(file_constants)
                 writer_constants.writerow([con_count, tok, 'type'])
+            # write to tokens file
+            writer.writerow([token_count, line, tok, '', 'id', '', 101])
             con_count += 1
 
         elif token_type == 'LAB':
-            writer.writerow([token_count, line, tok, '', '', 'id', 102])
             # write to LAB file
             with open(tables_path + program_file_name + '/LAB.csv', 'a', newline='') as file_labels:
                 writer_labels = csv.writer(file_labels)
                 writer_labels.writerow([lab_count, tok])
+            # write to tokens file
+            writer.writerow([token_count, line, tok, '', '', 'id', 102])
             lab_count += 1
         else:
             writer.writerow([token_count, line, tok, '', '', '', 'id'])
