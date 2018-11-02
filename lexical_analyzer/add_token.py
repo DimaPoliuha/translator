601cd3c749df82add3a042c3f36871c3e50d1f9f
DmitryPoliuha <dmitry.polyuha@gmail.com>
2018-11-02 18:06:54 +0200

    add back writing to separate files

--- lexical_analyzer/add_token.py
+++ lexical_analyzer/add_token.py
@@ -30,8 +30,11 @@ def add_token(tok, token_type, program_file_name, line):
             raise Exception('Re-announcement')
         elif not announcements_block and tok not in identifiers:
             raise Exception('Use of undeclared identifier')
+        # add IDN
         if announcements_block:
             identifiers.append(tok)
+            with open(tables_path + program_file_name + '/IDN.csv', 'a', newline='') as file_identifiers:
+                csv.writer(file_identifiers).writerow([len(identifiers) - 1, identifiers[-1]])
         tokens.append([tok_count, line, tok, identifiers.index(tok), '', '', tokens_identifiers[token_type]])
 
     elif token_type == 'CON':
@@ -39,19 +42,23 @@ def add_token(tok, token_type, program_file_name, line):
         for i in range(len(constants)):
             if tok in constants[i].keys():
                 con_id = i
-        for con in constants:
-            if tok in con.keys():
                 con_exists = True
                 break
+        # add CON
         if not con_exists:
             # define const type
             con_type = 'float' if '.' in tok else 'int'
             constants.append({tok: con_type})
+            with open(tables_path + program_file_name + '/CONST.csv', 'a', newline='') as file_constants:
+                csv.writer(file_constants).writerow([len(constants) - 1, tok, con_type])
         tokens.append([tok_count, line, tok, '', con_id, '', tokens_identifiers[token_type]])
 
     elif token_type == 'LAB':
+        # add LAB
         if tok not in labels:
             labels.append(tok)
+            with open(tables_path + program_file_name + '/LAB.csv', 'a', newline='') as file_labels:
+                csv.writer(file_labels).writerow([len(labels) - 1, labels[-1]])
         tokens.append([tok_count, line, tok, '', '', labels.index(tok), tokens_identifiers[token_type]])
 
     # write token to file
