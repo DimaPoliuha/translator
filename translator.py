c7ce4af784a5f938961932fc2dc6b45011a0dc5e
DmitryPoliuha <dmitry.polyuha@gmail.com>
2018-11-14 22:55:41 +0200

    Update translator.py

--- translator.py
+++ translator.py
@@ -1,6 +1,8 @@
+import sys
 from lexical_analyzer.analyzer import generate_tokens
 from syntactical_analyzer.parser import parser
 
 if __name__ == "__main__":
     generate_tokens()
-    parser()
+    if not parser():
+        sys.exit(-2)
