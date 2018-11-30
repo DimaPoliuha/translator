821a5dc36e505a2e797a99fb47eb7296d60c69d3
DmitryPoliuha <dmitry.polyuha@gmail.com>
2018-11-30 16:14:07 +0200

    Update create_tables_files.py

--- lexical_analyzer/create_tables_files.py
+++ lexical_analyzer/create_tables_files.py
@@ -1,23 +1,13 @@
-import csv
-
 root_dir = './'
 tables_path = root_dir + 'tables/'
 
 
 def create_tables_files(program_file_name):
-    # file to write tokens
-    with open(tables_path + program_file_name + '/tokens.csv', 'w', newline='') as f:
-        writer = csv.writer(f)
-        writer.writerow(['Token number', 'Line number', 'Token', 'IDN id', 'CON id', 'LAB id', 'TOK id'])
-    # file to write identifiers
-    with open(tables_path + program_file_name + '/IDN.csv', 'w', newline='') as f:
-        writer = csv.writer(f)
-        writer.writerow(['Id', 'Name', 'Value', 'Type'])
-    # file to write constants
-    with open(tables_path + program_file_name + '/CONST.csv', 'w', newline='') as f:
-        writer = csv.writer(f)
-        writer.writerow(['Id', 'Value', 'Type'])
-    # file to write labels
-    with open(tables_path + program_file_name + '/LAB.csv', 'w', newline='') as f:
-        writer = csv.writer(f)
-        writer.writerow(['Id', 'Name'])
+    with open(tables_path + program_file_name + '/tokens.csv', 'w', newline=''):
+        pass
+    with open(tables_path + program_file_name + '/IDN.csv', 'w', newline=''):
+        pass
+    with open(tables_path + program_file_name + '/CONST.csv', 'w', newline=''):
+        pass
+    with open(tables_path + program_file_name + '/LAB.csv', 'w', newline=''):
+        pass
