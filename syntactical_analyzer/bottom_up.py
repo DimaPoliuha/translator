64ac32e2c6489ec47e1dd26ce6b72a6cebef1009
DmitryPoliuha <dmitry.polyuha@gmail.com>
2018-12-10 14:59:19 +0200

    Update bottom_up.py

--- syntactical_analyzer/bottom_up.py
+++ syntactical_analyzer/bottom_up.py
@@ -89,3 +89,40 @@ grammar = {
         "'!='",
     ],
 }
+
+
+def grammar_parser():
+    bottom_up_array = []
+
+    for rule in grammar:
+        for rule_variant in grammar[rule]:
+            for tkn in rule_variant.split():
+                bottom_up_array.append(tkn)
+                # print(tkn.strip("'"))
+
+    bottom_up_array.append('program')
+    bottom_up_array = sorted(set(bottom_up_array))
+    bottom_up_array.reverse()
+    print(bottom_up_array)
+    
+    bottom_up_table = [[None for item in bottom_up_array] for it in bottom_up_array]
+
+    for rule in grammar:
+        for rule_variant in grammar[rule]:
+            rule_variant_tokens = rule_variant.split()
+            if len(rule_variant_tokens) > 1:
+                for i in range(len(rule_variant_tokens)):
+                    print(rule_variant_tokens[i])
+                print('|')
+        print()
+
+    # print(bottom_up_array.index('user_output'))
+    # for i in range(len(a)):
+    #     for j in range(len(a[i])):
+    #         print(a[i][j], end=' ')
+    #     print()
+    return bottom_up_table
+
+
+def parser(tokens):
+    main_table = grammar_parser()
