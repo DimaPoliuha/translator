757252f936f25af988a3cbcc3d11ceeb512ad976
DmitryPoliuha <dmitry.polyuha@gmail.com>
2019-04-23 22:37:23 +0300

    Update poliz_processing.py

--- translator_components/poliz/poliz_processing.py
+++ translator_components/poliz/poliz_processing.py
@@ -21,6 +21,9 @@ class PolizProcessing:
             'and': (lambda a, b: a and b),
             'or': (lambda a, b: a or b),
         }
+        self.poliz_auxiliary = [
+            'UPH', 'OUTE', 'INE', 'BP', 'OUTS', 'INS', 'EA'
+        ]
 
     def __call__(self, program_file):
         self.program_file = program_file
@@ -44,70 +47,93 @@ class PolizProcessing:
         return value
 
     def run_program(self):
+        for const in self.program_file.tokens.constants:
+            if const.con_type is 'int':
+                const.token = int(const.token)
+            elif const.con_type is 'float':
+                const.token = float(const.token)
+
         stack = []
         type_stack = []
         token_out = False
         token_in = False
-        for token in self.poliz:
-            print(token, type(token))
+        announcement_block = True
+        # for token in self.poliz:
+        #     print(token, type(token))
         for i, token in enumerate(self.poliz):
             curr_token = str(token)
+            print(curr_token)
 
-            if curr_token in self.op_1:
-                operand = self.get_operand(stack.pop())
-                result = self.op_1[curr_token](operand)
-                stack.append(result)
-                print(result)
-            elif curr_token in self.op_2:
-                operand_r = self.get_operand(stack.pop())
-                operand_l = self.get_operand(stack.pop())
-                result = self.op_2[curr_token](operand_l, operand_r)
-                stack.append(result)
-                print(result)
-            else:
-                stack.append(token)
-
-            # cin
-            if curr_token == 'INS':
-                token_in = True
-            elif curr_token == 'INE':
-                token_in = False
-            elif token_in:
-                #input
-                value = input()
-                value_type = self.program_file.tokens.identifiers[token.idn_id].idn_type
-                if value_type == 'int':
-                    if value.isdigit():
-                        self.program_file.tokens.identifiers[token.idn_id].value = int(value)
-                    else:
-                        raise TypeError('expected int')
-                elif value_type == 'float':
-                    if str(float(value)) == value:
-                        self.program_file.tokens.identifiers[token.idn_id].value = float(value)
-                    elif str(int(value)) == value:
-                        self.program_file.tokens.identifiers[token.idn_id].value = float(value)
-                    else:
-                        raise TypeError('expected float')
+            if announcement_block:
+                if curr_token == 'EA':
+                    announcement_block = False
+                # identifier type
+                if curr_token == 'int':
+                    for token_identifier in type_stack:
+                        self.program_file.tokens.identifiers[token_identifier.idn_id].idn_type = 'int'
+                    type_stack = []
+                elif curr_token == 'float':
+                    for token_identifier in type_stack:
+                        self.program_file.tokens.identifiers[token_identifier.idn_id].idn_type = 'float'
+                    type_stack = []
                 else:
-                    raise TypeError('strange type of identifier')
-
-            # cout
-            if curr_token == 'OUTS':
-                token_out = True
-            elif curr_token == 'OUTE':
-                token_out = False
-            elif token_out:
-                #output
-                print(str(self.program_file.tokens.identifiers[token.idn_id].value))
-
-            # identifier type
-            if curr_token == 'int':
-                for token_identifier in type_stack:
-                    self.program_file.tokens.identifiers[token_identifier.idn_id].idn_type = 'int'
-                type_stack = []
-            elif curr_token == 'float':
-                for token_identifier in type_stack:
-                    self.program_file.tokens.identifiers[token_identifier.idn_id].idn_type = 'float'
-                type_stack = []
+                    type_stack.append(token)
             else:
-                type_stack.append(token)
+                # cin
+                if curr_token == 'INS':
+                    token_in = True
+                elif curr_token == 'INE':
+                    token_in = False
+                elif token_in:
+                    # input
+                    value = input()
+                    value_type = self.program_file.tokens.identifiers[token.idn_id].idn_type
+                    if value_type == 'int':
+                        if value.isdigit():
+                            self.program_file.tokens.identifiers[token.idn_id].value = int(value)
+                        else:
+                            raise TypeError('expected int')
+                    elif value_type == 'float':
+                        if str(float(value)) == value:
+                            self.program_file.tokens.identifiers[token.idn_id].value = float(value)
+                        elif str(int(value)) == value:
+                            self.program_file.tokens.identifiers[token.idn_id].value = float(value)
+                        else:
+                            raise TypeError('expected float')
+                    else:
+                        raise TypeError('strange type of identifier')
+                    continue
+
+                # cout
+                if curr_token == 'OUTS':
+                    token_out = True
+                elif curr_token == 'OUTE':
+                    token_out = False
+                elif token_out:
+                    # output
+                    print(str(self.program_file.tokens.identifiers[token.idn_id].value))
+                    continue
+
+                if curr_token in self.op_1:
+                    pass
+                #     operand = self.get_operand(stack.pop())
+                #     result = self.op_1[curr_token](operand)
+                #     stack.append(result)
+                #     print(result)
+                # elif curr_token in self.op_2:
+                #     operand_r = self.get_operand(stack.pop())
+                #     operand_l = self.get_operand(stack.pop())
+                #     result = self.op_2[curr_token](operand_l, operand_r)
+                #     stack.append(result)
+                #     print(result)
+                else:
+                    if type(token) is not str:
+                        if not token.idn_id == '':
+                            stack.append(self.program_file.tokens.identifiers[token.idn_id].value)
+                        elif not token.con_id == '':
+                            stack.append(self.program_file.tokens.identifiers[token.con_id].value)
+                    elif curr_token not in self.poliz_auxiliary:
+                        print(curr_token)
+                        stack.append(curr_token)
+
+        print(stack)
